7978e0b626bd8b7bf226025731b9be8a099c22ae
Geoffrey Couten <geoffrey.couten@gmail.com>
2019-09-04 17:46:05 +0200

    Updates

---
+++ precache-manifest.74d63424af604af1c55a9764d5e13860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cb1eb9ca6fd221254e9cabaff8638d",
+    "url": "/simple-react-color-generator/index.html"
+  },
+  {
+    "revision": "57f47e48a6b5b80e2a40",
+    "url": "/simple-react-color-generator/static/css/main.c95577c9.chunk.css"
+  },
+  {
+    "revision": "6f38b5b491f69941416b",
+    "url": "/simple-react-color-generator/static/js/2.e5d445cf.chunk.js"
+  },
+  {
+    "revision": "57f47e48a6b5b80e2a40",
+    "url": "/simple-react-color-generator/static/js/main.47cce27a.chunk.js"
+  },
+  {
+    "revision": "7659f89cc7f5bb330776",
+    "url": "/simple-react-color-generator/static/js/runtime~main.ba0b5188.js"
+  }
+]);
